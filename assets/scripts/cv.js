ea3d14c4d5f31c77d916d67333809c00bd4f97b9
Jonnywdev <jonnywhitt19@gmail.com>
2022-06-13 12:30:17 +0000

    add initial cv page

--- assets/scripts/cv.js
+++ assets/scripts/cv.js
@@ -1,11 +1,6 @@
-const navGrow = document.querySelector('.extend-nav');
-const toggle = document.querySelector('#toggle');
 const cvGrow = document.querySelector('.extend-nav-cv');
 const cvToggle = document.querySelector('#cv-toggle');
 
-toggle.addEventListener('click', () => {
-    navGrow.classList.toggle('show-nav');
-})
 
 cvToggle.addEventListener('click', () => {
     cvGrow.classList.toggle('cv-nav-show');
